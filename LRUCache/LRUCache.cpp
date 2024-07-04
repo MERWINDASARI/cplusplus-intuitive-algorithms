a51b122cbd41508cd24d6ca5c2cfe2c1be6af12d
MERWINDASARI <dasarimerwinmighty@gmail.com>
2024-07-04 15:34:40 +0530

    done

--- LRUCache/LRUCache.cpp
+++ LRUCache/LRUCache.cpp
@@ -76,9 +76,3 @@ int main(){
     obj->put(6,6);
     return 0;
 }
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
